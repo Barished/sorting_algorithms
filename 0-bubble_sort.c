f6040fa631b08a6f17487e10d288d5707d1f38a5
Barished <Shedrackipi@gmail.com>
2023-07-24 15:48:21 +0000

    bubble sort updated

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,31 +1,35 @@
 #include "sort.h"
+#include <stdio.h>
 
 /**
- * bubble_sort - sort an array by bubbling through in ascending order
- * @array:array to be sorted
- * @size: size of the array
+ * bubble_sort - sorts an array of integers in ascending order
+ *
+ * @array: The array of integer
+ * @size: The size of the array
+ *
+ * Return: void
  */
-
 void bubble_sort(int *array, size_t size)
 {
-	size_t n = size;
-	size_t i, j;
-	int swap;
+	size_t i;
+	int tmp, sorted;
 
-	if (array != NULL)
-	{
-		for (i = 0; i < n - 1; i++)
+	if (!array || size == 0)
+		return;
+
+	do {
+		sorted = 1;
+		for (i = 0; i < size - 1; i++)
 		{
-			for (j = 0; j < n - i - 1; j++)
+			if (array[i] > array[i + 1])
 			{
-				if (array[j] > array[j + 1])
-				{
-					swap = array[j];
-					array[j] = array[j + 1];
-					array[j + 1] = swap;
-				}
+				sorted = 0;
+				tmp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = tmp;
+				print_array(array, size);
 			}
-			print_array(array, size);
 		}
-	}
+
+	} while (sorted == 0);
 }
